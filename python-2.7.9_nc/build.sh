2a9cb957fce85bdf7b296a0e4ae77feb3b51d2a7
Kolja Glogowski <kolja@pixie.de>
2014-12-22 18:37:01 +0100

    Added --no-as-needed linker flag to the Python builds.
    This flag makes sure that the DT_NEEDED entries for libncursesw
    and/or libtinfo is included in the readline extension module, in
    case the libreadline.so is underlinked.

--- python-2.7.9_nc/build.sh
+++ python-2.7.9_nc/build.sh
@@ -7,7 +7,7 @@ set -e
     --with-tcltk-includes="-I$PREFIX/include" \
     --with-tcltk-libs="-L$PREFIX/lib -ltcl8.5 -ltk8.5" \
     CPPFLAGS="-I$PREFIX/include" \
-    LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib"
+    LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
 
 make
 make install
