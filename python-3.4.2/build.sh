947a482ce97d1e9e64c1753092f4f5578912c047
Kolja Glogowski <kolja@pixie.de>
2014-12-22 03:44:07 +0100

    Python 3.4.2 recipes

---
+++ python-3.4.2/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+./configure --prefix=$PREFIX \
+    --enable-shared --enable-ipv6 \
+    --with-tcltk-includes="-I$PREFIX/include" \
+    --with-tcltk-libs="-L$PREFIX/lib -ltcl8.5 -ltk8.5" \
+    CPPFLAGS="-I$PREFIX/include" \
+    LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib"
+
+make
+make install
